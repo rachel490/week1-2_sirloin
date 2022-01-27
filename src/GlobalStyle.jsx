1f193f7ebc2533b61c4b20d38604eb825d7b5a87
gy <gayunlee11@gmail.com>
2022-01-27 20:55:18 +0900

    Chore: CRA 세팅 추가

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+
+  button {
+    background-color: transparent;
+    border: none;
+  }
+
+  ul, li {
+    padding: 0;
+    list-style: none;
+  }
+
+  input {
+    border: none;
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyle';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <GlobalStyle />
+  </ThemeProvider>,
+  document.getElementById('root'),
 );
