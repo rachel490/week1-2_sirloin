c10e54306855299e0fff2dccc09e2121c930f649
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-01-29 01:02:08 +0900

    Feat: 상품 혜택 허용 설정 컴포넌트

---
+++ src/components/containers/MileageStatus/index.js
@@ -0,0 +1,24 @@
+import { SettingFrame, SettingFrameItem } from 'components/atoms';
+import ToggleBtn from 'components/atoms/ToggleBtn';
+import React, { useState } from 'react';
+
+function MileageStatus() {
+  const [mileageStatus, setMileageStatus] = useState({ mileageStatus: false });
+
+  function onCheckedChange() {
+    setMileageStatus({ mileageStatus: !mileageStatus.mileageStatus });
+  }
+
+  return (
+    <SettingFrame title="상품 혜택 허용 설정">
+      <SettingFrameItem title="마일리지 적립">
+        <ToggleBtn
+          checked={mileageStatus.mileageStatus}
+          onChange={() => onCheckedChange()}
+        />
+      </SettingFrameItem>
+    </SettingFrame>
+  );
+}
+
+export default MileageStatus;
