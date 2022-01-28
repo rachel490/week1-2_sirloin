4f96df9b33ce82dcc73b1eece75f53ed0b5128cb
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-01-28 23:05:48 +0900

    Fix: 토글 버튼 사이즈 프랍 추가

--- src/components/atoms/ToggleBtn.jsx
+++ src/components/atoms/ToggleBtn.jsx
@@ -1,38 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-function ToggleBtn() {
-  return <ToggleButton type="checkbox" />;
+function ToggleBtn({ size }) {
+  return <ToggleButton type="checkbox" size={size} />;
 }
 
 const ToggleButton = styled.input`
-    width:80px;
-    height:30px;
+    width:${({ size }) => 53 * ((10 + size) / 10)}px;
+    height:${({ size }) => 20 * ((10 + size) / 10)}px;
     -webkit-appearance:none;
     outline:none;
     border-radius: 20px;
-    background: rgba(158,158,158);
+    background: rgb(158,158,158);
     transition: .2s;
     &:checked{
-        background: rgba(173,171,227);
+        background: rgb(173,171,227);
         ::before{
-            left:40px;
-            background:rgba(38,34,181);
+            left: ${({ size }) => 32 * ((10 + size) / 10)}px;
+            background:rgb(38,34,181);
         }
     }
     ::before{
         content: '';   
         position:absolute;
-        width: 30px;
-        height:30px;
+        width: ${({ size }) => 20 * ((10 + size) / 10)}px;
+        height:${({ size }) => 20 * ((10 + size) / 10)}px;
         border-radius: 20px;
         top: 0;
         left: 0;
-        background: rgba(250,250,250);
+        background: rgb(250,250,250);
         transform: scale(1.7);
-        box-shadow: 0 2px 5px rgba(0,0,0,.2);
+        box-shadow: 0 2px 5px rgb(0,0,0,.2);
         transition: .2s;
-    }
+    }    
 `;
+
+ToggleBtn.propTypes = {
+  size: PropTypes.number,
+};
+
+ToggleBtn.defaultProps = {
+  size: 1,
+};
 
 export default ToggleBtn;
