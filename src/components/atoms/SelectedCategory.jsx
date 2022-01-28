7a8e25834c72057ba1685cc64beed49f5a9c55cc
gy <gayunlee11@gmail.com>
2022-01-28 17:29:09 +0900

    Feat: 선택한 카테고리명 삭제 기능 추가

--- src/components/atoms/SelectedCategory.jsx
+++ src/components/atoms/SelectedCategory.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { AiOutlineClose } from 'react-icons/ai';
 
-function SelectedCategory({ func }) {
+function SelectedCategory({ state, categoryName, func }) {
   const onClose = () => {
-    func();
+    func(state);
   };
 
   return (
     <Wrapper>
       <Container>
-        <CategoryName>선택된 카테고리명</CategoryName>
+        <p>{categoryName}</p>
         <AiOutlineClose onClick={onClose} />
       </Container>
     </Wrapper>
@@ -19,12 +20,13 @@ function SelectedCategory({ func }) {
 }
 
 SelectedCategory.propTypes = {
+  state: PropTypes.arrayOf(PropTypes.array),
+  categoryName: PropTypes.string,
   func: PropTypes.func.isRequired,
 };
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
 `;
 
@@ -32,20 +34,17 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin: 0 0 0.5rem 0;
+  width: 11rem;
   padding: 0.5rem 0.8rem 0.5rem 1rem;
   background-color: rgb(232,232,239);
   border: 1px solid purple;
   border-radius: 0.3rem;
-
   & > svg {
     margin-left: 0.3rem;
     font-size: 0.9rem;
     cursor: pointer;
   }
-`;
-
-const CategoryName = styled.p`
-  font-size: 0.85rem;
 `;
 
 export default SelectedCategory;
