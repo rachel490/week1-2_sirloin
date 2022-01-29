97d2393f04299615354c483a448037d425317a76
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-01-29 15:36:13 +0900

    Fix: 컨테이너즈 디렉토리 정리

--- src/components/containers/ProductOption/index.js
+++ src/components/containers/ProductOption/index.js
@@ -27,18 +27,21 @@ function ProductOption() {
       isBackground
       isButton
       onClick={Clickhandler}
-      onChange={handleChange}
-    >
-      {optSets.length > 0
-        ? optSets.map((optSet) => <OptSet key={optSet.id} optSet={optSet} />)
-        : <EmptyOptSets><span>옵션세트를 추가하여 옵션을 구성해 주세요.</span></EmptyOptSets>}
+      onChange={handleChange}>
+      {optSets.length > 0 ? (
+        optSets.map((optSet) => <OptSet key={optSet.id} optSet={optSet} />)
+      ) : (
+        <EmptyOptSets>
+          <span>옵션세트를 추가하여 옵션을 구성해 주세요.</span>
+        </EmptyOptSets>
+      )}
     </SettingFrame>
   );
 }
 
 const EmptyOptSets = styled.div`
   display: flex;
-  justify-content: center;  
+  justify-content: center;
   align-items: center;
   width: 100%;
   height: 500px;
