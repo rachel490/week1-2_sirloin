23ea05d274838aaeea218bbf3ce9c63e90f88caf
rachel490 <ccori12@naver.com>
2022-01-29 11:20:11 +0900

    Fix: 구매자 추천 이미지 이름 수정 및 props 추가

--- src/components/containers/RecommendedImage/index.jsx
+++ src/components/containers/RecommendedImage/index.jsx
@@ -1,19 +1,21 @@
 import ImageUpload from 'components/atoms/ImageUpload';
 import SettingFrame from 'components/atoms/SettingFrame';
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-function IntroImage() {
+function RecommendedImage() {
+  const [images, setImages] = useState([]);
+
   return (
     <Wrap>
       <SettingFrame title="구매자 추천 이미지">
-        <ImageUpload />
+        <ImageUpload imgFiles={images} setImgFiles={setImages} />
       </SettingFrame>
     </Wrap>
   );
 }
 
-export default IntroImage;
+export default RecommendedImage;
 
 const Wrap = styled.div`
   margin:50px;
