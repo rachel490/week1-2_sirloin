f0b817a254ff57ea46ef704ca0eba0ac2bd1aa4e
rachel490 <ccori12@naver.com>
2022-01-29 10:59:25 +0900

    Feat: SalesDisplayTerm에recoil 추가

--- src/components/containers/SalesDisplayTerm.jsx
+++ src/components/containers/SalesDisplayTerm.jsx
@@ -1,14 +1,39 @@
+/* eslint-disable no-console */
 import SettingFrame from 'components/atoms/SettingFrame';
 import Term from 'components/molecules/Term';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { isSavedState, registerFormState } from 'utils/globalState';
 
 function SalesDisplayTerm() {
+  const [displayTerm, setDisplayTerm] = useState('제한 없음');
+  const [salesTerm, setSalesTerm] = useState('제한 없음');
+
+  const isSaved = useRecoilValue(isSavedState);
+  const [registerForm, setRegisterForm] = useRecoilState(registerFormState);
+
+  const saveData = (key, value) => {
+    setRegisterForm({
+      ...registerForm,
+      [key]: value,
+    });
+  };
+
+  console.log(registerForm);
+
+  useEffect(() => {
+    saveData('노출 및 판매기간 설정', {
+      노출기한: displayTerm,
+      판매기한: salesTerm,
+    });
+  }, [isSaved]);
+
   return (
     <Wrap>
       <SettingFrame title="노출 및 판매기간 설정">
-        <Term name="노출" />
-        <Term name="판매" />
+        <Term name="노출" select={displayTerm} setSelect={setDisplayTerm} />
+        <Term name="판매" select={salesTerm} setSelect={setSalesTerm} />
       </SettingFrame>
     </Wrap>
   );
